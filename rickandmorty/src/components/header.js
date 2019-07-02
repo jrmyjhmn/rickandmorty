926d078991319cd11c43ba30dc2cc8230bc89b46
Jeremy Jahman <jeremyjahman@iMac-van-gracious-2.local>
2019-07-02 16:13:12 +0200

    started project

---
+++ rickandmorty/src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style/header.scss'
+
+class Header extends React.Component {
+    
+    render() {
+        return (
+            <div className="header">
+                <h1>gracious - developers case</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
