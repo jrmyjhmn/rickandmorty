89de88aa32f0bfef16c855ce236f0f6ed2cffb34
Jeremy Jahman <jeremyjahman@iMac-van-gracious-2.local>
2019-07-04 17:32:57 +0200

    hallo

--- rickandmorty/src/components/locations.js
+++ rickandmorty/src/components/locations.js
@@ -5,9 +5,7 @@ class Location extends React.Component {
     constructor() {
         super()
         this.state = {
-            id: null,
-            locations: [],
-            residentsEarth: []
+            locations:[],
         }
         this.handleClick = this.handleClick.bind(this)
     }
@@ -16,32 +14,31 @@ class Location extends React.Component {
         fetch('https://rickandmortyapi.com/api/location/')
         .then(results => results.json())
         .then(data => {
-            console.log(data)
             this.setState({
                 locations: data.results,
-                residentsEarth: data.results[0].residents
+                locationId: data.results.id
             })
-            return data
+        return data
         })
-
     }
 
-    handleClick() {
+    handleClick(e) {
+
+        console.log(e.target)
+        console.log(this.state.locations[3])
 
     }
 
     render() {
 
-        const { locations, residentsEarth } = this.state
+        const { locations } = this.state
         const locationList = locations.map(location => <li onClick={this.handleClick} key={location.name+1}>{location.name}</li>)
-        const characterList = residentsEarth.map(resident => <li key={resident+1}>{resident}</li>)
-        console.log(residentsEarth)
+        
         console.log(locations)
 
         return(
             <div>
                 <div>{locationList}</div>
-                <div>{characterList}</div>
             </div>
         )
     }
